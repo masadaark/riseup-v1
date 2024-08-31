a12976437ce45e4c6bcf45653bda0d29b8920673
Mark <masadaark@gmail.com>
2024-08-31 19:09:57 +0700

    nocodeanswer

--- riseup-v1/src/logic/DepositLogic.tsx
+++ riseup-v1/src/logic/DepositLogic.tsx
@@ -1,17 +1,20 @@
-import dayjs from "dayjs";
 import { DepositTableInterface } from "../components/table/DepositTable.component";
+
+const round2DecimalPlace = (num:number):number=>{
+    return Math.round(num * 100)/100
+}
 
 // write code hear
 export class DepositLogic {
     public static GenerateDepositTableLogic(
-        monthlyDeposit: number ,
-        increateDepositPercent: number ,
-        annualReturnPercent: number ,
-        startYear: number ,
-        endYear: number 
+        monthlyDeposit: number,
+        increateDepositPercent: number,
+        annualReturnPercent: number,
+        startYear: number,
+        endYear: number
     ) {
         const data: DepositTableInterface[] = [];
-
+        
         return data
     }
 }
